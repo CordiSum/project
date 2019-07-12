19630cd760e3c9805186294ee2ed69adb59b49f5
Dimas <dmitriyberkut@gmail.com>
2019-07-12 17:50:02 +0300

    ex_1

--- ex_1/script.js
+++ ex_1/script.js
@@ -3,17 +3,12 @@ var time = prompt("Введите дату в формате YYYY-MM-DD");
 
 var appData = {
 
-    budget: money;
-    timeData: time;
-    expenses: {
-        askExpenses: prompt( "Введите обязательную статью расходов в этом месяце?");
-        aksOptionalExpenses: prompt( "Во сколько обойдется?");
-    };
-
-    optionalExpenses: {};
-    income: [];
-    savings: false;
-
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
 }
 
 alert("Ваш бюджет на день составит:" + (money/30) );
