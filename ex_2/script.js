377b8459cf5d893c3d67f859be0f06f61662a1b3
Dimas <dmitriyberkut@gmail.com>
2019-07-14 16:17:52 +0300

    3 types of cycles and ex_2

--- ex_2/script.js
+++ ex_2/script.js
@@ -21,7 +21,8 @@ for (let i = 0; i < 2; i++) {
         console.log('done');
         appData.expenses[a] = b;
     } else {
-
+        console.log ("bad result");
+        i--;
     }    
 };
 // ======================
@@ -36,7 +37,8 @@ for (let i = 0; i < 2; i++) {
 //         console.log('done');
 //         appData.expenses[a] = b;
 //     } else {
-
+//         console.log ("bad result");
+//         i--;
 //     } 
 
 //     i++;
@@ -53,7 +55,8 @@ for (let i = 0; i < 2; i++) {
 //         console.log('done');
 //         appData.expenses[a] = b;
 //     } else {
-
+//         console.log ("bad result");
+//         i--;
 //     } 
 
 //     i++;
