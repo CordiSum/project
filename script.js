b33b30311a2c268e93acd128e006680330eceaa8
Dimas <dmitriyberkut@gmail.com>
2019-07-22 13:35:13 +0300

    15 lesson - Array

--- script.js
+++ script.js
@@ -230,4 +230,48 @@
 
 // ============================================
 
+// 15. Массивы и псевдомассивы ================================
 
+
+// let arr = [1, 2, 3, 4, 5];
+
+// arr.pop(); 
+// arr.push('5');
+// arr.shift();
+// arr.unshift('1');
+
+// console.log(arr);
+
+// Перебор массива через for  ---------------------
+
+// let arr = [1, 2, 3, 4, 5];
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// console.log(arr);
+
+// Перебор массива через forEach ---------------------
+
+// let arr = ['first', 2, 3, 'four', 5];
+
+// arr.forEach( function(item, i, mass) {
+//     console.log(i + ': ' + item + '(массив: '+ mass + ')' );
+// } );
+
+//  Перебор For off (перебирает тот элемент, который берем)---------
+
+// let mass = [1, 3, 4, 6, 7];
+
+// for (let key of mass) {
+//     console.log(key);
+// }
+
+// Примеры ---------------------------------------------
+
+let ans = prompt('', ''),
+    arr = [];
+
+arr = ans.split(',');
+console.log(arr);
