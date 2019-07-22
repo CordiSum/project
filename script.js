5628ed238d01c8a6d7567865f1de66d944f3f064
Dimas <dmitriyberkut@gmail.com>
2019-07-22 11:42:07 +0300

    13 lesson - callback

--- script.js
+++ script.js
@@ -53,7 +53,7 @@
 // }
 //  ===========================================
 
-// 13. Callback-функции =======================
+// 12. Функции =======================
 // let num = 20;
 // function showFirstMessage(text) {
 //     alert(text);
@@ -160,3 +160,45 @@
 //     console.log('example increment', c1, c2, c3)
 
 // =============================================
+
+// 13. Callback-Функции =======================
+
+
+// function first() {
+//     setTimeout( function() {
+//     console.log(1);
+//     }, 500);
+// }
+
+// function second() {
+//     console.log(2);
+// }
+
+// first();
+// second();
+// ------------------------------
+
+// function learnJS(lang, callback) {
+//     console.log('Я учу ' + lang);
+//     callback();
+// }
+
+// learnJS('JavaScript', function() {
+//     console.log('Я прошел 3-й урок');
+// });
+// ---------------------------------
+
+// function learnJS(lang, callback) {
+//     console.log('Я учу ' + lang);
+//     callback();
+// }
+
+// function done() {
+//     console.log('Я прошел 3-й урок');    
+// }
+// learnJS('JavaScript', done);
+
+// ============================================
+
+// 14. Объекты ================================
+
