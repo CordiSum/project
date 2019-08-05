de4e8af2ca9db09ab353f9d6e07754ce0a349b23
Dimas <dmitriyberkut@gmail.com>
2019-08-05 15:49:51 +0300

    18 lessons JS Dynamic Typing

--- script.js
+++ script.js
@@ -319,3 +319,54 @@
 // console.log(john.armor);
 
 // ============================================
+
+// 18. Динамическая типизация в JS ============
+
+// - to String
+// 1) 
+console.log(typeof(String(null)));
+console.log(typeof(String(4)));
+
+// 2) Конкатенация
+console.log('ww' + 'err');
+console.log('ww' + 5);
+console.log( typeof('q' + 5));
+console.log( typeof('q' + false));
+console.log( typeof('' + false));
+console.log('http://vk.com/catalog/' + 5);
+
+// - to Number
+
+// 1) 
+console.log(typeof(Number('5')));
+
+// 2) унарный плюс 
+console.log(typeof(+'5'));
+console.log(typeof(5 + +'5'));
+
+// 3)
+console.log(typeof(parseInt('15px', 10)));
+let ans = +prompt('Hello', '');
+
+// 4) 
+// 0, '', null, undefined, NaN    это всегда false
+
+// let switcher = null;
+
+// if (switcher) {
+//     console.log('Working..')
+// }
+
+// switcher = 1;
+
+// if (switcher) {
+//     console.log('Working..')
+// }
+
+// 5) Преобразование в тип Boolean 
+// console.log(typeof(Boolean('5')));
+
+// // 6)
+// console.log(typeof(!!'5'));
+
+// ============================================
