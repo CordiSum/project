426654e7d98b70e7576ae4b0ee6ae5c595edd593
Berkut_Dima <dmitriyb@wizardsdev.com>
2019-09-23 18:13:27 +0300

    not end

---
+++ yoga/js/script.js
@@ -0,0 +1,16 @@
+window.addEventListener('DOMContentLoaded', function() {
+    'use strict';
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('info-tabcontent');
+
+    function hideTabContent(a) {
+        for (let i = a; a < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
+    hideTabContent(1);
+
+    function showTabContent
+});
