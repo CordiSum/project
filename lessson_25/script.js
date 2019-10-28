7cb69628baa9a4a7b9ce3d18dd50a3f3fbf559bc
Berkut_Dima <dmitriyb@wizardsdev.com>
2019-10-28 18:26:18 +0200

    lessons 25

--- lessson_25/script.js
+++ lessson_25/script.js
@@ -1,5 +1,16 @@
 let box = document.querySelector('.box');
+let btn = document.querySelector('button');
 
+let width = box.clientWidth;
+let height = box.clientHeight;
 
-let width = box.clientWidth,
-    height = box.clientHeight;
+console.log(box.getBoundingClientRect().left);
+console.log(document.documentElement.clientWidth);
+
+btn.addEventListener('click', function() {
+    box.style.height = box.scrollHeight + 'px';
+})
+
+scrollBy(0, 200);  //перемещает на 200px вниз
+
+scrollTo(0, 200) // перемещает к координатам 200px сверху
