cb75e56909fec9e24c61ecaba726269419c6e2f2
Berkut_Dima <dmitriyb@wizardsdev.com>
2019-11-05 11:56:44 +0200

    include masonry JS

--- DBG/app/src/js/main.js
+++ DBG/app/src/js/main.js
@@ -2,35 +2,31 @@ import 'webp-in-css/polyfill';
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    // init Isotope
+// init Isotope
 var $grid = $('.DBG__wrap-item').isotope({
-    itemSelector: '.DBG__wrap-item__block',
-    layoutMode: 'fitRows',
-    getSortData: {
-      category: '[data-category]',
-      weight: function( itemElem ) {
-        var weight = $( itemElem ).find('.weight').text();
-        return parseFloat( weight.replace( /[\(\)]/g, '') );
-      }
-    }
-  });  
-  
-  // bind filter button click
-  $('#filters').on( 'click', '.DBG__wrap-menu__nav-button', function() {
-    var filterValue = $( this ).attr('data-filter');
-    // use filterFn if matches value
-    filterValue = filterFns[ filterValue ] || filterValue;
-    $grid.isotope({ filter: filterValue });
+  itemSelector: '.DBG__wrap-item__block',
+  layoutMode: 'fitRows',
+});
+
+// bind filter button click
+$('#filters').on( 'click', '.DBG__wrap-menu__nav-li', function() {
+  var filterValue = $( this ).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
+});
+
+// bind sort button click
+$('#sorts').on( 'click', '.DBG__wrap-menu__nav-li', function() {
+  var sortByValue = $(this).attr('data-sort-by');
+  $grid.isotope({ sortBy: sortByValue });
+});
+
+// change is-checked class on buttons
+$('.button-group').each( function( i, buttonGroup ) {
+  var $buttonGroup = $( buttonGroup );
+  $buttonGroup.on( 'click', '.DBG__wrap-menu__nav-li', function() {
+    $buttonGroup.find('.is-checked').removeClass('is-checked');
+    $( this ).addClass('is-checked');
   });
-  
-  
-  // change is-checked class on buttons
-  $('.DBG__wrap-menu__nav').each( function( i, buttonGroup ) {
-    var $buttonGroup = $( buttonGroup );
-    $buttonGroup.on( 'click', '.DBG__wrap-menu__nav-button', function() {
-      $buttonGroup.find('.is-checked').removeClass('is-checked');
-      $( this ).addClass('is-checked');
-    });
-  });
+});
   
 });
