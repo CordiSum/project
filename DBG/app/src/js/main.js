3067279b783c60dfc8662b1526d1b11cf7220ac0
Berkut_Dima <dmitriyb@wizardsdev.com>
2019-11-05 12:16:51 +0200

    clean js masonry

--- DBG/app/src/js/main.js
+++ DBG/app/src/js/main.js
@@ -2,31 +2,16 @@ import 'webp-in-css/polyfill';
 
 document.addEventListener('DOMContentLoaded', () => {
 
-// init Isotope
-var $grid = $('.DBG__wrap-item').isotope({
-  itemSelector: '.DBG__wrap-item__block',
-  layoutMode: 'fitRows',
-});
-
-// bind filter button click
-$('#filters').on( 'click', '.DBG__wrap-menu__nav-li', function() {
-  var filterValue = $( this ).attr('data-filter');
-  $grid.isotope({ filter: filterValue });
-});
-
-// bind sort button click
-$('#sorts').on( 'click', '.DBG__wrap-menu__nav-li', function() {
-  var sortByValue = $(this).attr('data-sort-by');
-  $grid.isotope({ sortBy: sortByValue });
-});
-
-// change is-checked class on buttons
-$('.button-group').each( function( i, buttonGroup ) {
-  var $buttonGroup = $( buttonGroup );
-  $buttonGroup.on( 'click', '.DBG__wrap-menu__nav-li', function() {
-    $buttonGroup.find('.is-checked').removeClass('is-checked');
-    $( this ).addClass('is-checked');
+  // init Isotope
+  var $grid = $('.DBG__wrap-item').isotope({
+    itemSelector: '.DBG__wrap-item__block',
+    layoutMode: 'fitRows',
   });
-});
+
+  // bind filter button click
+  $('#DGB_filters').on( 'click', '.DBG__wrap-menu__nav-li', function() {
+    var filterValue = $( this ).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+  });
   
 });
