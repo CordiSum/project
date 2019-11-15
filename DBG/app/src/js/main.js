9749371961a3b32ab9a12f1a12c98f5abeb9b4f9
Berkut_Dima <dmitriyb@wizardsdev.com>
2019-11-15 18:14:46 +0200

    beta-test v0.0.1

--- DBG/app/src/js/main.js
+++ DBG/app/src/js/main.js
@@ -22,7 +22,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
     $('#DBG__myslider').slick({
       arrows: true,
-      dots: true
+      dots: true,
+      adaptiveHeight: true,
+      responsive: [
+        {
+          breakpoint: 1054,
+          settings: {
+            arrows: false,
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
     });
 
 
