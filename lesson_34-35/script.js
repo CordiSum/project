d22d7972093c7adc6f58c4b1b79e76cbbd89dbfa
Dimas <dmitriyberkut@gmail.com>
2020-03-12 08:45:32 +0200

    lesson_34

---
+++ lesson_34-35/script.js
@@ -0,0 +1,13 @@
+let options = {
+    width: 1366,
+    height: 768,
+    background: 'red',
+    font: {
+        size: '16',
+        color: '#fff'
+    }
+};
+
+console.log(JSON.parse(JSON.stringify(options)));
+
+
