c209ac422e2cbe5115d6df481d85bcac8d36e2ad
Berkut_Dima <dmitriyb@wizardsdev.com>
2020-08-20 15:28:50 +0300

    lesson 19 - callback function

--- UPD_training/lessons/script.js
+++ UPD_training/lessons/script.js
@@ -11,4 +11,29 @@ const logg = "Hello world";
 console.log(logg.slice(6, 11));
 
 const num = "12.2px";
-const test = parseInt(num); 
+const test = parseInt(num);
+
+// callback function
+
+function first() {
+    //DO something
+    setTimeout(function() {
+        console.log(1);
+    }, 1000);
+}
+function second() {
+    //DO something
+        console.log(2);
+}
+first();
+second();
+
+function learnJS(lang, callback) {
+    console.log(`Я учу ${lang}`);
+    callback();
+}
+
+function done() {
+    console.log('Я прошел этот урок!');
+}
+learnJS('JS', done);
